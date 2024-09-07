4fdb0dedc32c584120d30c2e4e7154d0095120ea
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2024-09-07 09:28:33 +0700

    feat: parse routing

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,7 +1,24 @@
 import {BlobReader, Entry, TextWriter, ZipReader} from "@zip.js/zip.js";
 import Papa from "papaparse";
 
-export async function test() {
+export class DefaultDict<T, Q> extends Map<T, Q> {
+    defaultFactory: () => Q
+    constructor(defaultFactory: () => Q) {
+      super()
+      this.defaultFactory = defaultFactory
+    }
+    get(name: T): Q {
+      if (this.has(name)) {
+        return super.get(name)!
+      } else {
+        const value = this.defaultFactory()
+        this.set(name, value)
+        return value
+      }
+    }
+  }
+
+export async function getRawData() {
     const response = await fetch('/assets/file_gtfs.zip');
     const blob = await response.blob()
     const reader = new BlobReader(blob);
@@ -13,24 +30,81 @@ export async function test() {
         fileEntries[entry.filename] = entry;
     }
 
-    const routesWriter = new TextWriter();
-    const routesEntry = fileEntries['routes.txt'];
-    const routesText = await routesEntry.getData(routesWriter);
-    const routes = Papa.parse(routesText, {
-        header: true,
-    });
+    const getFileRawData = async (filename: string) => {
+        const entry = fileEntries[filename];
+        const writer = new TextWriter();
+        const text = await entry.getData(writer);
+        return Papa.parse(text, {
+            header: true,
+        });
+    };
+    
+    const [routesRawData, stopsRawData, shapesRawData, tripsRawData] = await Promise.all([
+        getFileRawData('routes.txt'),
+        getFileRawData('stops.txt'),
+        getFileRawData('shapes.txt'),
+        getFileRawData('trips.txt'),
+    ]);
 
-    const stopsWriter = new TextWriter();
-    const stopsEntry = fileEntries['stops.txt'];
-    const stopsText = await stopsEntry.getData(stopsWriter);
-    const stops = Papa.parse(stopsText, {
-        header: true,
-    });
+    if (routesRawData.error) {
+        console.error(routesRawData.error);
+    }
+    
+    if (stopsRawData.error) {
+        console.error(stopsRawData.error);
+    }
+    
+    if (shapesRawData.error) {
+        console.error(shapesRawData.error);
+    }
+    
+    if (tripsRawData.error) {
+        console.error(tripsRawData.error);
+    }
 
     await zipReader.close();
 
     return {
-        routes,
-        stops,
-    }
+        routesRawData: routesRawData.data as Array<RouteRawData>,
+        stopsRawData: stopsRawData.data as Array<StopRawData>,
+        shapesRawData: shapesRawData.data as Array<ShapeRawData>,
+        tripsRawData: tripsRawData.data as Array<TripRawData>,
+    };
 }
+
+export type RouteRawData = {
+    id: string;
+    agency_id: string;
+    route_short_name: string;
+    route_long_name: string;
+    route_desc: string;
+    route_color: string;
+    route_text_color: string;
+};
+
+export type StopRawData = {
+    stop_id: string;
+    stop_name: string;
+    stop_lat: number;
+    stop_lon: number;
+    zone_id: any;
+    location_type: any;
+    wheelchair_boarding: any;
+};
+
+export type ShapeRawData = {
+    shape_id: string;
+    shape_pt_lat: number;
+    shape_pt_lon: number;
+    shape_pt_sequence: number;
+};
+
+export type TripRawData = {
+    route_id: string;
+    service_id: string;
+    trip_id: string;
+    trip_headsign: string;
+    direction_id: number;
+    shape_id: string;
+    trip_short_name: string;
+};
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,8 +1,19 @@
 import {Component, onMount} from 'solid-js';
 import maplibregl from 'maplibre-gl';
 import {jakartaCoordinate} from "../constants";
-import {test} from "../consumer";
+import {DefaultDict, getRawData, RouteRawData, ShapeRawData} from "../consumer";
 import style from "./map.module.scss";
+
+type Trip = {
+    routeId: string;
+    shapes: Array<ShapeRawData>;
+};
+
+type Route = {
+    data: RouteRawData,
+    // stops: Array<Stop>,
+    trips: Array<Trip>,
+};
 
 export const Map: Component = () => {
     onMount(async () => {
@@ -13,25 +24,90 @@ export const Map: Component = () => {
             zoom: 12,
         });
 
-        const {routes, stops} = await test();
+        const {routesRawData, stopsRawData, tripsRawData, shapesRawData} = await getRawData();
 
-        map.addSource('bus_stops', {
+        const shapeSegments = new DefaultDict<string, Array<ShapeRawData>>(() => []);
+        for (const shapeRawData of shapesRawData) {
+            shapeSegments.get(shapeRawData.shape_id).push(shapeRawData);
+        }
+        for (const shapes of Object.values(shapeSegments) as Array<Array<ShapeRawData>>) {
+            shapes.sort((shape) => shape.shape_pt_sequence);
+        }
+
+        const trips = new DefaultDict<string, Array<Trip>>(() => []);
+        for (const tripRawData of tripsRawData) {
+            trips.get(tripRawData.route_id).push({
+                routeId: tripRawData.trip_id,
+                shapes: shapeSegments.get(tripRawData.shape_id),
+            });
+        }
+
+        const routes: Record<string, Route> = {};
+        for (const routeRawData of routesRawData) {
+            const routeTrips = [];
+
+            routes[routeRawData.id] = {
+                data: routeRawData,
+                // stops: [],
+                trips: trips[routeRawData.id],
+            };
+        }
+
+        for (const [routeId, route] of Object.entries(routes)) {
+            const stopCoordinates = route.trips.map((trip) => [trip.latitude, trip.longitude]);
+
+            map.addSource(`route_${routeId}`, {
+                type: 'geojson',
+                data: {
+                    type: 'FeatureCollection',
+                    features: [
+                        {
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': stopCoordinates,
+                            }
+                        }
+                    ],
+                },
+            });
+        }
+
+        const stopCoordinates = stopsRawData.map((stopRawData) => [stopRawData.stop_lon, stopRawData.stop_lat]);
+        map.addSource(`opentije_bus_stops`, {
             type: 'geojson',
             data: {
                 type: 'FeatureCollection',
-                features: stops.data.map(stop => ({
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point',
-                        coordinates: [stop.stop_lon, stop.stop_lat],
+                features: [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'MultiPoint',
+                            'coordinates': stopCoordinates,
+                        }
                     }
-                })),
+                ],
             },
         });
+
+        // const stopsFeatures = stops.data.map(stop => ({
+        //     type: 'Feature',
+        //     geometry: {
+        //         type: 'Point',
+        //         coordinates: [stop.stop_lon, stop.stop_lat],
+        //     }
+        // }));
+        // map.addSource('bus_stops', {
+        //     type: 'geojson',
+        //     data: {
+        //         type: 'FeatureCollection',
+        //         features: stopsFeatures,
+        //     },
+        // });
         map.addLayer({
-            id: 'bus_stops',
+            id: 'opentije_bus_stops',
             type: 'circle',
-            source: 'bus_stops'
+            source: 'opentije_bus_stops'
         });
     });
 
