1fe362eaa0c3455ca5a2b2076ef071a7edece500
AkipTsaqif <akiptsaqif@gmail.com>
2024-10-19 16:29:07 +0700

    added ux feature for bus stop search such as differentiate between the searched stop and the rest of the stops

--- src/ui/home/map/map.tsx
+++ src/ui/home/map/map.tsx
@@ -6,7 +6,7 @@ import {
     NavigationControl,
 } from "maplibre-gl";
 import { jakartaCoordinate } from "../../../constants";
-import { useTransportData } from "../../../data/transport-data";
+import { BusStop, useTransportData } from "../../../data/transport-data";
 import { useSidebarState } from "../../../data/sidebar-state";
 
 export const MapCanvas: Component = () => {
@@ -14,6 +14,47 @@ export const MapCanvas: Component = () => {
         useTransportData();
     const { setIsExpanded } = useSidebarState();
     const [libreMap, setLibreMap] = createSignal<Map | null>(null);
+
+    const updateBusStopSources = (map: Map, selectedStopId: string | null) => {
+        const busStopsSource = map.getSource(
+            "opentije_bus_stops",
+        ) as GeoJSONSource;
+        const selectedStopSource = map.getSource(
+            "selected_bus_stop",
+        ) as GeoJSONSource;
+
+        if (selectedStopId) {
+            const selectedStop = geoData().busStops.find(
+                (stop) => stop.id === selectedStopId,
+            );
+
+            if (selectedStop) {
+                selectedStopSource.setData({
+                    type: "FeatureCollection",
+                    features: [selectedStop.geoJson],
+                });
+
+                const filteredSameStop = geoData()
+                    .busStops.filter((stop) => stop.id !== selectedStop.id)
+                    .map((stop) => stop.geoJson);
+
+                busStopsSource.setData({
+                    type: "FeatureCollection",
+                    features: filteredSameStop,
+                });
+            }
+        } else {
+            selectedStopSource.setData({
+                type: "FeatureCollection",
+                features: [],
+            });
+
+            busStopsSource.setData({
+                type: "FeatureCollection",
+                features: geoData().busStops.map((stop) => stop.geoJson),
+            });
+        }
+    };
 
     createEffect(() => {
         const map = libreMap();
@@ -38,31 +79,23 @@ export const MapCanvas: Component = () => {
         });
 
         const entry = selectedEntry();
-        if (entry) {
-            if (entry.category === "Bus Stop") {
-                const selectedStop = geoData().busStops.find(
-                    (stop) => stop.id === entry.id,
-                );
-                if (selectedStop) {
-                    map.flyTo({
-                        center: [selectedStop.longitude, selectedStop.latitude],
-                        zoom: 16,
-                        speed: 0.5,
-                        curve: 2.5,
-                    });
-                }
-            } else {
+        if (entry && entry.category === "Bus Stop") {
+            updateBusStopSources(map, entry.id);
+
+            const selectedStop = geoData().busStops.find(
+                (stop) => stop.id === entry.id,
+            );
+
+            if (selectedStop) {
                 map.flyTo({
-                    center: [
-                        jakartaCoordinate.longitude,
-                        jakartaCoordinate.latitude,
-                    ],
-                    zoom: 11,
+                    center: [selectedStop.longitude, selectedStop.latitude],
+                    zoom: 16,
                     speed: 0.5,
                     curve: 2.5,
                 });
             }
         } else {
+            updateBusStopSources(map, null);
             map.flyTo({
                 center: [
                     jakartaCoordinate.longitude,
@@ -106,7 +139,7 @@ export const MapCanvas: Component = () => {
                 id: "opentije_bus_stops-label",
                 type: "symbol",
                 source: "opentije_bus_stops",
-                minzoom: 13,
+                minzoom: 15,
                 layout: {
                     "text-field": ["get", "name"],
                     "text-font": ["Noto Sans Regular"],
@@ -119,6 +152,7 @@ export const MapCanvas: Component = () => {
                 id: "opentije_bus_stops",
                 type: "circle",
                 source: "opentije_bus_stops",
+                minzoom: 12,
                 paint: {
                     "circle-color": ["get", "color"],
                 },
@@ -141,9 +175,47 @@ export const MapCanvas: Component = () => {
                 },
             });
 
+            map.addSource("selected_bus_stop", {
+                type: "geojson",
+                data: {
+                    type: "FeatureCollection",
+                    features: [],
+                },
+            });
+
+            map.addLayer({
+                id: "selected_bus_stop",
+                type: "circle",
+                source: "selected_bus_stop",
+                paint: {
+                    "circle-color": "#FF0000",
+                    "circle-radius": 10,
+                },
+            });
+
+            map.addLayer({
+                id: "selected_bus_stop-label",
+                type: "symbol",
+                source: "selected_bus_stop",
+                layout: {
+                    "text-field": ["get", "name"],
+                    "text-font": ["Noto Sans Bold"],
+                    "text-size": 20,
+                    "text-variable-anchor": ["left", "right"],
+                    "text-radial-offset": 0.8,
+                    "text-justify": "auto",
+                },
+                paint: {
+                    "text-color": "#000000",
+                    "text-opacity": 1,
+                },
+            });
+
             map.on("click", "opentije_bus_stops", (target) => {
                 const busStopId = target.features![0].properties.id;
+
                 setSelectedBusStop(busStopId);
+                updateBusStopSources(map, busStopId);
             });
 
             setLibreMap(map);
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         solidPlugin(),
     ],
     server: {
-        port: 3001,
+        port: 3000,
     },
     build: {
         target: "esnext",
